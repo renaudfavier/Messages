fc884a2eb24b7fdc82ab3eea04dc7238232a04f6
Renaud Favier <renaud.favier.blimer@gmail.com>
2025-10-24 22:27:44 +0200

    Setup basic DI

---
+++ app/src/main/java/com/renaudfavier/messages/MessagesApplication.kt
@@ -0,0 +1,7 @@
+package com.renaudfavier.messages
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MessagesApplication: Application()
---
+++ app/src/main/java/com/renaudfavier/messages/chat/di/ChatModule.kt
@@ -0,0 +1,9 @@
+package com.renaudfavier.messages.chat.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface ChatModule
