9994b9829ccdc6a6ab1b50ec9420252f50607e8d
Renaud Favier <renaud.favier.blimer@gmail.com>
2025-10-25 22:36:33 +0200

    Use mutableStateFlow in repository

--- app/src/main/java/com/renaudfavier/messages/chat/data/FakeMessageRepository.kt
+++ app/src/main/java/com/renaudfavier/messages/chat/data/FakeMessageRepository.kt
@@ -6,48 +6,51 @@ import com.renaudfavier.messages.chat.domain.MessageRepository
 import com.renaudfavier.messages.core.domain.ContactId
 import com.renaudfavier.messages.core.domain.toContactId
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class FakeMessageRepository @Inject constructor(): MessageRepository {
 
-    private val messages = sampleMessages.toMutableList()
+    private val messages = MutableStateFlow(sampleMessages)
 
     override fun getAllConversations(): Flow<List<ContactId>> {
         val currentUserId = 0.toContactId()
-        val contactIds = messages
-            .flatMap { listOf(it.author, it.recipient) }
-            .filter { it != currentUserId }
-            .distinct()
-            .sortedBy { contactId ->
-                messages
-                    .filter { it.author == contactId || it.recipient == contactId }
-                    .maxOfOrNull { it.date }
-            }
-            .reversed()
-
-        return flowOf(contactIds)
+        return messages.map { messageList ->
+            messageList
+                .flatMap { listOf(it.author, it.recipient) }
+                .filter { it != currentUserId }
+                .distinct()
+                .sortedBy { contactId ->
+                    messageList
+                        .filter { it.author == contactId || it.recipient == contactId }
+                        .maxOfOrNull { it.date }
+                }
+                .reversed()
+        }
     }
 
     override fun getConversationFlow(contactId: ContactId): Flow<List<Message>> {
-        val conversationMessages = messages
-            .filter {
-                it.author == contactId|| it.recipient == contactId
-            }
-            .sortedBy { it.date }
-
-        return flowOf(conversationMessages)
+        return messages.map { messageList ->
+            messageList
+                .filter {
+                    it.author == contactId || it.recipient == contactId
+                }
+                .sortedBy { it.date }
+        }
     }
 
     override suspend fun sendMessage(message: Message): Result<Unit> {
-        messages.add(message)
+        messages.value = messages.value + message
         return Result.success(Unit)
     }
 
     override suspend fun messageWasRead(id: MessageId): Result<Unit> {
-        val messageIndex = messages.indexOfFirst { it.id == id }
+        val messageIndex = messages.value.indexOfFirst { it.id == id }
         if (messageIndex != -1) {
-            messages[messageIndex] = messages[messageIndex].copy(isUnread = false)
+            messages.value = messages.value.toMutableList().apply {
+                set(messageIndex, get(messageIndex).copy(isUnread = false))
+            }
             return Result.success(Unit)
         }
         return Result.failure(Error("Message not found"))
