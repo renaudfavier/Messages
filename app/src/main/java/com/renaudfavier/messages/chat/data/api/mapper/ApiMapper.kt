f50cee46c8269df99b5439e086d830ad1b7eb1da
Renaud Favier <renaud.favier.blimer@gmail.com>
2025-10-26 10:17:48 +0100

    Add avatars

--- app/src/main/java/com/renaudfavier/messages/chat/data/api/mapper/ApiMapper.kt
+++ app/src/main/java/com/renaudfavier/messages/chat/data/api/mapper/ApiMapper.kt
@@ -1,13 +1,12 @@
 package com.renaudfavier.messages.chat.data.api.mapper
 
+import com.renaudfavier.messages.R
 import com.renaudfavier.messages.chat.data.api.model.ChatDto
 import com.renaudfavier.messages.chat.data.api.model.MessageDto
 import com.renaudfavier.messages.chat.domain.Message
 import com.renaudfavier.messages.core.domain.Contact
-import com.renaudfavier.messages.core.domain.ContactId
 import com.renaudfavier.messages.core.domain.toContactId
 import java.time.Instant
-import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 class ApiMapper @Inject constructor() {
@@ -16,7 +15,13 @@ class ApiMapper @Inject constructor() {
         return Contact(
             id = chat.id.toContactId(),
             name = chat.name,
-            avatar = com.renaudfavier.messages.R.drawable.ic_launcher_foreground // Default avatar
+            avatar = when(chat.id) {
+                "34f9bb23-c25a-4e77-9948-51434cc156b2" -> R.drawable.peter
+                "4ccb26d2-22f9-4a58-ba91-ba00bd008bb9" -> R.drawable.sarah
+                "74d072a3-2d41-43df-92a0-235659f941ff" -> R.drawable.john
+                "ffb2b56d-b39d-4045-a4bc-dd03746a7d7f" -> R.drawable.lea
+                else -> R.drawable.ic_launcher_foreground
+            } // Default avatar
         )
     }
 
